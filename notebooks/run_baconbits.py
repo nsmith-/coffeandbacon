05111921284707d324bb325b2db4e5753a9a4e0a
Nick Smith <nick.smith@cern.ch>
2019-03-14 22:46:27 -0500

    More factorizing

--- notebooks/run_baconbits.py
+++ notebooks/run_baconbits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
-from collections import defaultdict, OrderedDict
+import collections
 import warnings
 import concurrent.futures
 import gzip
@@ -11,6 +11,7 @@ import time
 import numexpr
 
 import uproot
+import awkward
 import numpy as np
 from fnal_column_analysis_tools import hist, lookup_tools
 
@@ -65,138 +66,163 @@ hists['opposite_ak8_msd_SR'] = hist.Hist("Events", dataset, gencat, jetpt_coarse
 hists['opposite_ak4_leadingDeepCSV_SR'] = hist.Hist("Events", dataset, gencat, jetpt_coarse, jetmass_coarse, hist.Bin("opposite_ak4_leadingDeepCSV", "Max(DeepCSV) (of $\leq4$ leading)", 40, 0, 1))
 hists['njets_ak4_SR'] = hist.Hist("Events", dataset, gencat, jetpt_coarse, jetmass_coarse, hist.Bin("nAK4PuppijetsPt30", "Number AK4 Jets", 8, 0, 8))
 
-branches = [
-    "AK8Puppijet0_pt",
-    "AK8Puppijet0_eta",
-    "AK8Puppijet0_msd",
-    "AK8Puppijet0_isHadronicV",
-    "AK8Puppijet0_deepdoubleb",
-    "AK8Puppijet0_deepdoublec",
-    "AK8Puppijet0_deepdoublecvb",
-    "AK8Puppijet0_N2sdb1",
-    "AK8Puppijet0_isTightVJet",
-    # "nAK4PuppijetsPt30dR08_0",
-    "nAK4PuppijetsPt30",
-    "npu",
-    "scale1fb",
-    "kfactorEWK",
-    "kfactorQCD",
-    "neleLoose",
-    "nmuLoose",
-    "ntau",
-    "pfmet",
-    "AK8Puppijet0_phi",
-    "AK8Puppijet1_phi",
-    "AK8Puppijet1_e4_v2_sdb1",
-    "AK8Puppijet1_e3_v1_sdb1",
-    "AK8Puppijet1_tau32",
-    "AK8Puppijet1_msd",
-    "AK4Puppijet0_dPhi08",
-    "AK4Puppijet1_dPhi08",
-    "AK4Puppijet2_dPhi08",
-    "AK4Puppijet3_dPhi08",
-    "AK4Puppijet0_deepcsvb",
-    "AK4Puppijet1_deepcsvb",
-    "AK4Puppijet2_deepcsvb",
-    "AK4Puppijet3_deepcsvb",
-]
-
 
 def clean(val, default):
     val[np.isnan(val)|(val==-999.)] = default
     return val
 
 
-def processfile(dataset, file):
-    # Many 'invalid value encountered in ...' due to pt and msd sometimes being zero
-    # This will just fill some NaN bins in the histogram, which is fine
-    isData = dataset=="data_obs"
-    fin = uproot.open(file)
-    skim_sumw = None
-    if "otree" in fin:
-        tree = fin["otree"]
-        if not isData:
-            skim_sumw  = fin["SumWeights"].values[0]
-    else:
-        tree = fin["Events"]
-    arrays = tree.arrays(branches, namedecode='ascii')
-    tic = time.time()
-
+def process(df):
+    isData = df['dataset'] == "data_obs"
     # jet |eta|<2.5 sometimes gives no events
     # or other cuts in: https://github.com/DAZSLE/BaconAnalyzer/blob/102x/Analyzer/src/VJetLoader.cc#L270-L272
-    arrays["AK8Puppijet0_pt"] = clean(arrays["AK8Puppijet0_pt"], 0.001)
-    arrays["AK8Puppijet0_N2sdb1"] = clean(arrays["AK8Puppijet0_N2sdb1"], np.inf)
+    df["AK8Puppijet0_pt"] = clean(df["AK8Puppijet0_pt"], 0.001)
+    df["AK8Puppijet0_N2sdb1"] = clean(df["AK8Puppijet0_N2sdb1"], np.inf)
 
     # we'll take care of cross section later, just check if +/-1
-    genW = np.sign(arrays["scale1fb"])
+    genW = np.sign(df["scale1fb"])
     weight = genW
     if dataset in corrections['2017_pileupweight_dataset']:
-        weight *= corrections['2017_pileupweight_dataset'][dataset](arrays["npu"])
+        weight *= corrections['2017_pileupweight_dataset'][dataset](df["npu"])
     if 'ZJetsToQQ_HT' in dataset or 'WJetsToQQ_HT' in dataset:
-        weight *= arrays["kfactorEWK"] * arrays["kfactorQCD"]
-    weight *= corrections['2017_trigweight_msd_pt'](arrays["AK8Puppijet0_msd"], arrays["AK8Puppijet0_pt"])
-    weight *= (arrays["AK8Puppijet0_pt"] > 200)
+        weight *= df["kfactorEWK"] * df["kfactorQCD"]
+    weight *= corrections['2017_trigweight_msd_pt'](df["AK8Puppijet0_msd"], df["AK8Puppijet0_pt"])
+    weight *= (df["AK8Puppijet0_pt"] > 200)
 
-    weight_SR = weight * ((arrays["neleLoose"]==0) & (arrays["nmuLoose"]==0) & (arrays["ntau"]==0))
+    weight_SR = weight * ((df["neleLoose"]==0) & (df["nmuLoose"]==0) & (df["ntau"]==0))
 
-    arrays["AK8Puppijet0_msd"] *= corrections['msdweight'](arrays["AK8Puppijet0_pt"], arrays["AK8Puppijet0_eta"])
-    arrays["jetrho"] = 2*np.log(np.maximum(1e-4, arrays["AK8Puppijet0_msd"]/arrays["AK8Puppijet0_pt"]))
-    arrays["AK8Puppijet0_N2sdb1_ddt"] = arrays["AK8Puppijet0_N2sdb1"] - corrections['2017_n2ddt_rho_pt'](arrays["jetrho"], arrays["AK8Puppijet0_pt"])
-    weight_SR *= (arrays["AK8Puppijet0_N2sdb1_ddt"] < 0) & (arrays["AK8Puppijet0_isTightVJet"]!=0)
-    weight_metCut = (arrays["pfmet"] < 140.)
+    df["AK8Puppijet0_msd"] *= corrections['msdweight'](df["AK8Puppijet0_pt"], df["AK8Puppijet0_eta"])
+    df["jetrho"] = 2*np.log(np.maximum(1e-4, df["AK8Puppijet0_msd"]/df["AK8Puppijet0_pt"]))
+    df["AK8Puppijet0_N2sdb1_ddt"] = df["AK8Puppijet0_N2sdb1"] - corrections['2017_n2ddt_rho_pt'](df["jetrho"], df["AK8Puppijet0_pt"])
+    weight_SR *= (df["AK8Puppijet0_N2sdb1_ddt"] < 0) & (df["AK8Puppijet0_isTightVJet"]!=0)
+    weight_metCut = (df["pfmet"] < 140.)
 
-    e4_v2_jet1 = clean(arrays['AK8Puppijet1_e4_v2_sdb1'], 1.)
-    e3_v1_jet1 = clean(arrays['AK8Puppijet1_e3_v1_sdb1'], -1.)
-    dphi = np.unwrap(arrays['AK8Puppijet1_phi'] - arrays['AK8Puppijet0_phi'])
-    arrays['opposite_ak8_n3sdb1'] = np.where(np.abs(dphi) > np.pi/2., e4_v2_jet1/np.maximum(1e-4, e3_v1_jet1)**2, np.inf)
-    arrays['opposite_ak8_tau32'] = np.where(np.abs(dphi) > np.pi/2., arrays['AK8Puppijet1_tau32'], np.inf)
-    arrays['opposite_ak8_msd'] = np.where(np.abs(dphi) > np.pi/2., arrays['AK8Puppijet1_msd'], np.inf)
-    dphi04 = np.column_stack([arrays['AK4Puppijet%d_dPhi08' % i] for i in range(4)])
-    btag04 = np.column_stack([arrays['AK4Puppijet%d_deepcsvb' % i] for i in range(4)])
+    e4_v2_jet1 = clean(df['AK8Puppijet1_e4_v2_sdb1'], 1.)
+    e3_v1_jet1 = clean(df['AK8Puppijet1_e3_v1_sdb1'], -1.)
+    dphi = np.unwrap(df['AK8Puppijet1_phi'] - df['AK8Puppijet0_phi'])
+    df['opposite_ak8_n3sdb1'] = np.where(np.abs(dphi) > np.pi/2., e4_v2_jet1/np.maximum(1e-4, e3_v1_jet1)**2, np.inf)
+    df['opposite_ak8_tau32'] = np.where(np.abs(dphi) > np.pi/2., df['AK8Puppijet1_tau32'], np.inf)
+    df['opposite_ak8_msd'] = np.where(np.abs(dphi) > np.pi/2., df['AK8Puppijet1_msd'], np.inf)
+    dphi04 = np.column_stack([df['AK4Puppijet%d_dPhi08' % i] for i in range(4)])
+    btag04 = np.column_stack([df['AK4Puppijet%d_deepcsvb' % i] for i in range(4)])
     btag04[np.abs(dphi04)<np.pi/2] = -np.inf
-    arrays['opposite_ak4_leadingDeepCSV'] = np.max(btag04, axis=1)
+    df['opposite_ak4_leadingDeepCSV'] = np.max(btag04, axis=1)
 
     hout = {}
     for k in hists.keys():
         h = hists[k].copy(content=False)
+        fields = {k: df[k] for k in h.fields if k in df}
         if k == 'sumw':
-            if skim_sumw is None:
-                h.fill(dataset=dataset, sumw=genW)
+            if 'skim_sumw' in df:
+                h.fill(dataset=dataset, sumw=1, weight=df['skim_sumw'])
             else:
-                h.fill(dataset=dataset, sumw=1, weight=skim_sumw)
+                h.fill(dataset=dataset, sumw=genW)
         elif k == 'pfmet_nminus1_SR':
-            h.fill(dataset=dataset, **arrays, weight=weight_SR)
+            h.fill(**fields, weight=weight_SR)
         elif '_SR' in k:
-            h.fill(dataset=dataset, **arrays, weight=weight_SR*weight_metCut)
+            h.fill(**fields, weight=weight_SR*weight_metCut)
         else:
-            h.fill(dataset=dataset, **arrays, weight=weight)
+            h.fill(**fields, weight=weight)
         hout[k] = h
+
+    return hout
+
+
+class LazyArrays(collections.abc.MutableMapping):
+    def __init__(self, tree):
+        self._tree = tree
+        self._dict = self._tree.lazyarrays(namedecode='ascii')
+        self._materialized = set()
+
+    def __delitem__(self, key):
+        del self._dict[key]
+
+    def __getitem__(self, key):
+        if key in self._dict:
+            value = self._dict[key]
+            if isinstance(value, uproot.tree.LazyArray):
+                self._materialized.add(key)
+                return value[:]
+            return value
+        else:
+            raise KeyError(key)
+
+    def __iter__(self):
+        print("uh oh")
+        for item in self._dict:
+            self._materialized.add(item[0])
+            yield item
+
+    def __len__(self):
+        return len(self._dict)
+
+    def __setitem__(self, key, value):
+        self._dict[key] = value
+
+    @property
+    def materialized(self):
+        return self._materialized
+
+
+def processfile(dataset, file):
+    fin = uproot.open(file)
+    skim_sumw = None
+    if 'otree' in fin:
+        tree = fin['otree']
+        if 'SumWeights' in fin:
+            skim_sumw = fin['SumWeights'].values[0]
+    else:
+        tree = fin['Events']
+
+    df = LazyArrays(tree)
+    df['dataset'] = dataset
+    df['skim_sumw'] = skim_sumw
+    tic = time.time()
+
+    hout = process(df)
 
     toc = time.time()
     bytesread = fin.source.bytesread if isinstance(fin.source, uproot.source.xrootd.XRootDSource) else 0
-    return dataset, tree.numentries, hout, bytesread, toc-tic
+    output = {
+        'dataset': dataset,
+        'nentries': tree.numentries,
+        'histograms': hout,
+        'bytesread': bytesread,
+        'elapsedtime': toc-tic,
+        'columns': df.materialized,
+    }
+    return output
 
 
-test = True
+test = False
 
 tstart = time.time()
 for h in hists.values(): h.clear()
-nevents = defaultdict(lambda: 0.)
-nbytes = defaultdict(lambda: 0.)
+nevents = collections.defaultdict(lambda: 0.)
+nbytes = collections.defaultdict(lambda: 0.)
 sumworktime = 0.
+columns_accessed = set()
+
+def collect(output):
+    global sumworktime, columns_accessed
+    dataset = output['dataset']
+    nevents[dataset] += output['nentries']
+    nbytes[dataset] += output['bytesread']
+    sumworktime += output['elapsedtime']
+    for k, v in output['histograms'].items():
+        hists[k] += v
+    columns_accessed.update(output['columns'])
+
 
 if test:
     nworkers = 1
-    dataset = "TTToHadronic_TuneCP5_13TeV_powheg_pythia8"
-    # root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/skim/TTToHadronic_TuneCP5_13TeV_powheg_pythia8_0.root
-    file = "TTToHadronic_TuneCP5_13TeV_powheg_pythia8_0.root"
-    _, nentries, hout, nbytesds, dt = processfile(dataset, file)
-    nevents[dataset] += nentries
-    nbytes[dataset] += nbytesds
-    sumworktime += dt
-    for k in hout.keys():
-        hists[k] += hout[k]
-    print("Done processing test file")
+    testfiles = [
+        ("TTToHadronic_TuneCP5_13TeV_powheg_pythia8", "TTToHadronic_TuneCP5_13TeV_powheg_pythia8_0.root"),
+        ("TTToSemiLeptonic_TuneCP5_13TeV_powheg_pythia8", "TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0.root"),
+    ]
+    for i, (dataset, file) in enumerate(testfiles):
+        collect(processfile(dataset, file))
+        print("Done processing test file %d" % i)
 else:
     nworkers = 10
     #fileslice = slice(None, 5)
@@ -204,7 +230,7 @@ else:
     #with concurrent.futures.ThreadPoolExecutor(max_workers=nworkers) as executor:
     with concurrent.futures.ProcessPoolExecutor(max_workers=nworkers) as executor:
         futures = set()
-        samples = samplefiles["Hbb_create_2017"]
+        samples = samplefiles['Hbb_create_2017']
         for datasets in samples.values():
             if isinstance(datasets, list):
                 # raw data, no norm
@@ -219,12 +245,7 @@ else:
             while len(futures) > 0:
                 finished = set(job for job in futures if job.done())
                 for job in finished:
-                    dataset, nentries, hout, nbytesds, dt = job.result()
-                    nevents[dataset] += nentries
-                    nbytes[dataset] += nbytesds
-                    sumworktime += dt
-                    for k in hout.keys():
-                        hists[k] += hout[k]
+                    collect(job.result())
                     processed += 1
                     print("Processing: done with % 4d / % 4d files" % (processed, total))
                 futures -= finished
@@ -275,6 +296,9 @@ for ds in nevents.keys():
 for h in hists.values(): h.scale(scale, axis="dataset")
 
 dt = time.time() - tstart
+print("Columns accessed:")
+for col in sorted(list(columns_accessed)):
+    print("\t", col)
 print("%.2f us*cpu/event" % (1e6*dt*nworkers/sum(nevents.values()), ))
 print("%.2f us*cpu/event work time" % (1e6*sumworktime/sum(nevents.values()), ))
 nbins = sum(sum(arr.size for arr in h._sumw.values()) for h in hists.values())
