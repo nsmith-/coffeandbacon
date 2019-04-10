4b2ef8e072b0b415b0d43bd2e752bd738074be4f
Nick Smith <nick.smith@cern.ch>
2019-04-10 17:32:03 -0500

    Use some argparse

--- notebooks/run_baconbits.py
+++ notebooks/run_baconbits.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
-from __future__ import print_function, division
-import collections
-import warnings
 import gzip
 import pickle
 import json
 import time
 import cloudpickle
+import argparse
 
 import uproot
 import numpy as np
 from fnal_column_analysis_tools import hist, processor
 
+
 # instrument xrootd source
 def _read(self, chunkindex):
     self.bytesread = getattr(self, 'bytesread', 0) + self._chunkbytes
     return self._read_real(chunkindex)
+
 
 uproot.source.xrootd.XRootDSource._read_real = uproot.source.xrootd.XRootDSource._read
 uproot.source.xrootd.XRootDSource._read = _read
@@ -54,16 +54,22 @@ def process_file(dataset, file, processor_instance, stats_accumulator, preload_i
 
 
 if __name__ == '__main__':
-    test = True
-    executor = 'futures'
-    # executor = 'iterative'
-    nworkers = 1 if executor == 'iterative' else 2
-    preload_items = {}
-    output_filename = "hists.pkl.gz"
-    processor_instance_filename = "boostedHbbProcessor.cpkl.gz"
-    profile_html_filename = 'run_baconbits.html'
+    parser = argparse.ArgumentParser(description='Run analysis on baconbits files using processor cloudpickle files')
+    parser.add_argument('--processor', default='boostedHbbProcessor.cpkl.gz', help='The name of the compiled processor file')
+    parser.add_argument('--output', default='hists.pkl.gz', help='Output histogram filename')
+    parser.add_argument('--samplejson', default='metadata/samplefiles.json', help='JSON file containing dataset and file locations')
+    parser.add_argument('--sample', default='Hbb_create_2017', help='The sample to use in the sample JSON')
+    parser.add_argument('--limit', type=int, default=None, metavar='N', help='Limit to the first N files of each dataset in sample JSON')
+    parser.add_argument('--test', action='store_true', help='Process the test files defined in this execuatble')
+    parser.add_argument('--executor', choices=['iterative', 'futures'], default='iterative', help='The type of executor to use')
+    parser.add_argument('-j', '--workers', type=int, default=12, help='Number of workers to use for multi-worker executors (e.g. futures or condor)')
+    parser.add_argument('--profile-out', dest='profilehtml', default=None, help='Filename for the pyinstrument HTML profile output')
+    args = parser.parse_args()
 
-    if test:
+    # Set a list of preloaded columns, to profile the execution separately from the uproot deserialization
+    preload_items = {}
+
+    if args.test:
         filelist = [
             ("TTToHadronic_TuneCP5_13TeV_powheg_pythia8", "TTToHadronic_TuneCP5_13TeV_powheg_pythia8_0.root"),
             ("TTToSemiLeptonic_TuneCP5_13TeV_powheg_pythia8", "TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0.root"),
@@ -71,17 +77,16 @@ if __name__ == '__main__':
             ("SingleMuon", "SingleMuonRun2017B_17Nov2017_v1_2.root"),
         ]
     else:
-        with open("metadata/samplefiles.json") as fin:
+        with open(args.samplejson) as fin:
             samplefiles = json.load(fin)
-        samples = samplefiles['Hbb_create_2017']
+        samples = samplefiles[args.sample]
         filelist = []
         for group, datasets in samples.items():
             for dataset, files in datasets.items():
                 for file in files[:]:
                     filelist.append((dataset, file))
 
-
-    with gzip.open(processor_instance_filename, "rb") as fin:
+    with gzip.open(args.processor, "rb") as fin:
         processor_instance = cloudpickle.load(fin)
 
     combined_accumulator = processor.dict_accumulator({
@@ -100,16 +105,18 @@ if __name__ == '__main__':
         return processor.dict_accumulator({'stats': stats, 'job': out})
 
     tstart = time.time()
-    if executor == 'iterative':
-        from pyinstrument import Profiler
-        profiler = Profiler()
-        profiler.start()
+    if args.executor == 'iterative':
+        if args.profilehtml is not None:
+            from pyinstrument import Profiler
+            profiler = Profiler()
+            profiler.start()
         processor.iterative_executor(filelist, work_function, combined_accumulator)
-        profiler.stop()
-        with open(profile_html_filename, "w") as fout:
-            fout.write(profiler.output_html())
-    elif executor == 'futures':
-        processor.futures_executor(filelist, work_function, combined_accumulator, workers=nworkers)
+        if args.profilehtml is not None:
+            profiler.stop()
+            with open(args.profilehtml, "w") as fout:
+                fout.write(profiler.output_html())
+    elif args.executor == 'futures':
+        processor.futures_executor(filelist, work_function, combined_accumulator, workers=args.workers)
 
     final_accumulator = combined_accumulator['job']
     stats = combined_accumulator['stats']
@@ -121,13 +128,14 @@ if __name__ == '__main__':
     print("Read %.1fM bytes" % (stats['bytesread'].value/1e6, ))
 
     nbins = sum(sum(arr.size for arr in h._sumw.values()) for h in final_accumulator.values() if isinstance(h, hist.Hist))
-    nfilled = sum(sum(np.sum(arr>0) for arr in h._sumw.values()) for h in final_accumulator.values() if isinstance(h, hist.Hist))
+    nfilled = sum(sum(np.sum(arr > 0) for arr in h._sumw.values()) for h in final_accumulator.values() if isinstance(h, hist.Hist))
     print("Filled %.1fM bins" % (nbins/1e6, ))
     print("Nonzero bins: %.1f%%" % (100*nfilled/nbins, ))
 
     # Pickle is not very fast or memory efficient, will be replaced by something better soon
-    with gzip.open(output_filename, "wb") as fout:
+    with gzip.open(args.output, "wb") as fout:
         pickle.dump(final_accumulator, fout)
 
     dt = time.time() - tstart
+    nworkers = 1 if args.executor == 'iterative' else args.workers
     print("%.2f us*cpu/event overall" % (1e6*dt*nworkers/stats['nentries'].value, ))
