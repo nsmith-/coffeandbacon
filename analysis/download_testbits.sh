1b1ab841f1190676381856a0a0b24cc7feb2189c
Nick Smith <nick.smith@cern.ch>
2019-04-11 20:47:23 -0500

    Properly run on both skimmed and unskimmed bits
    Reduce nesting in sample json

--- analysis/download_testbits.sh
+++ analysis/download_testbits.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 mkdir -p data
+
+# skimmed bits
 xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/skim/TTToHadronic_TuneCP5_13TeV_powheg_pythia8_0.root data/
 xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.02/skim/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0.root data/
 xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/skim/JetHTRun2017F_17Nov2017_v1_24.rootnodupl.root data/
 xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/skim/SingleMuonRun2017B_17Nov2017_v1_2.root data/
+
+# unskimmed bits
+xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/GluGluHToBB_M125_LHEHpT_250_Inf_13TeV_amcatnloFXFX_pythia8/Output_job0_job0_file0to59.root data/GluGluHToBB_M125_LHEHpT_250_Inf_13TeV_amcatnloFXFX_pythia8.Output_job0_job0_file0to59.root
+xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/GluGluHToCC_M125_LHEHpT_250_Inf_13TeV_amcatnloFXFX_pythia8/Output_job15_job15_file900to959.root data/GluGluHToCC_M125_LHEHpT_250_Inf_13TeV_amcatnloFXFX_pythia8.Output_job15_job15_file900to959.root
+xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8/Output_job116_job116_file1160to1169.root data/QCD_HT700to1000_TuneCP5_13TeV-madgraphMLM-pythia8.Output_job116_job116_file1160to1169.root
+xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcbacon/dazsle/zprimebits-v15.01/TTToHadronic_TuneCP5_13TeV_powheg_pythia8/Output_job101_job101_file4040to4079.root data/TTToHadronic_TuneCP5_13TeV_powheg_pythia8.Output_job101_job101_file4040to4079.root
--- analysis/run_baconbits.py
+++ analysis/run_baconbits.py
@@ -58,9 +58,8 @@ if __name__ == '__main__':
     parser.add_argument('--processor', default='boostedHbbProcessor.cpkl.gz', help='The name of the compiled processor file')
     parser.add_argument('--output', default='hists.pkl.gz', help='Output histogram filename')
     parser.add_argument('--samplejson', default='metadata/samplefiles.json', help='JSON file containing dataset and file locations')
-    parser.add_argument('--sample', default='Hbb_create_2017', help='The sample to use in the sample JSON')
+    parser.add_argument('--sample', default='test_skim', help='The sample to use in the sample JSON')
     parser.add_argument('--limit', type=int, default=None, metavar='N', help='Limit to the first N files of each dataset in sample JSON')
-    parser.add_argument('--test', action='store_true', help='Process the test files defined in this execuatble')
     parser.add_argument('--executor', choices=['iterative', 'futures'], default='iterative', help='The type of executor to use')
     parser.add_argument('-j', '--workers', type=int, default=12, help='Number of workers to use for multi-worker executors (e.g. futures or condor)')
     parser.add_argument('--profile-out', dest='profilehtml', default=None, help='Filename for the pyinstrument HTML profile output')
@@ -69,22 +68,13 @@ if __name__ == '__main__':
     # Set a list of preloaded columns, to profile the execution separately from the uproot deserialization
     preload_items = {}
 
-    if args.test:
-        filelist = [
-            ("TTToHadronic_TuneCP5_13TeV_powheg_pythia8", "data/TTToHadronic_TuneCP5_13TeV_powheg_pythia8_0.root"),
-            ("TTToSemiLeptonic_TuneCP5_13TeV_powheg_pythia8", "data/TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_0.root"),
-            ("JetHT", "data/JetHTRun2017F_17Nov2017_v1_24.rootnodupl.root"),
-            ("SingleMuon", "data/SingleMuonRun2017B_17Nov2017_v1_2.root"),
-        ]
-    else:
-        with open(args.samplejson) as fin:
-            samplefiles = json.load(fin)
-        samples = samplefiles[args.sample]
-        filelist = []
-        for group, datasets in samples.items():
-            for dataset, files in datasets.items():
-                for file in files[:args.limit]:
-                    filelist.append((dataset, file))
+    with open(args.samplejson) as fin:
+        samplefiles = json.load(fin)
+    sample = samplefiles[args.sample]
+    filelist = []
+    for dataset, files in sample.items():
+        for file in files[:args.limit]:
+            filelist.append((dataset, file))
 
     with gzip.open(args.processor, "rb") as fin:
         processor_instance = cloudpickle.load(fin)
