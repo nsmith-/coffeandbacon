7a29904a00e1796decc6f39817452989fcb50c6d
Lindsey Gray <Lindsey.Gray@cern.ch>
2019-05-23 12:41:12 -0500

    correct linux version for unl

--- setup_parsl_unl.sh
+++ setup_parsl_unl.sh
@@ -1,6 +1,6 @@
 !/usr/bin/env bash
 
-echo 'source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc7-opt/setup.sh' >> ${HOME}/.bash_profile
+echo 'source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-slc6-gcc62-opt/setup.sh' >> ${HOME}/.bash_profile
 echo 'export PATH=${HOME}/.local/bin:$PATH' >> ${HOME}/.bash_profile
 echo 'export PYTHONPATH=${HOME}/.local/lib/python3.6/site-packages:$PYTHONPATH' >> ${HOME}/.bash_profile
 
